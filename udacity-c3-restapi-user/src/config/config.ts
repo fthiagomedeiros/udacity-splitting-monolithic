a558939267833550254663cbdf76b7900755cb24
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-02-19 22:43:59 -0300

    Refactoring User Service (#2)
    * refactor: removed feed service dependencies
    
    Splitting the monolithic application originally covering Feed and User services,
    I have removed all dependencies from Feed Service
    
    * chore: removed lock dependency
    
    * chore: Removed vuneralibility dependency

--- udacity-c3-restapi-user/src/config/config.ts
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -9,15 +9,7 @@ export const config = {
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_BUCKET
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  },
   "jwt": {
     "secret": process.env.JWT_SECRET
   }
-
-}
+};
